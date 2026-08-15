71ff5d998b0d86fbe753b7761fb52ff3001c9280
umeshhnandavja-byte <umeshhnanda.vja@gmail.com>
2026-08-15 19:40:28 +0530

    Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import Header from "./Header"
+import Header from "./Header";
 import Footer from "./Footer";
 import Food from "./Food";
+import Card from "./Card";
 
 function App() {
 
   return(
     <>
       <Header></Header>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
       <Food></Food>
       <Food></Food>
       <Food></Food>
