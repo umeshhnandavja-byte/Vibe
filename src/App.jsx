8aa84b2a8a4f0090f3ff63891e73f0023ca83cae
umeshhnandavja-byte <umeshhnanda.vja@gmail.com>
2026-08-17 16:54:43 +0530

    Props

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./Footer";
 import Food from "./Food";
 import Card from "./Card";
 import Button from "./Button";
+import Student from "./Student";
 
 function App() {
 
@@ -20,6 +21,11 @@ function App() {
       <Food></Food>
       <Food></Food>
       <Food></Food>
+      <Student name="Spongebob" age="30" isStudent={true}/>
+      <Student name="Patrick" age={42} isStudent={false}/>
+      <Student name="Squidward" age={50} isStudent={false}/>
+      <Student name="Sandy" age={27} isStudent={true}/>
+      <Student/>
       <Footer></Footer>
     </>
   ); 
---
+++ src/Student.jsx
@@ -0,0 +1,11 @@
+function Student({name = "Guest", age = 0, isStudent = false}){
+    return(
+        <div className="student">
+            <p>Name: {name}</p>
+            <p>Age: {age}</p>
+            <p>Student: { isStudent ? "Yes" : "No"}</p>
+        </div>
+    );
+}
+
+export default Student
