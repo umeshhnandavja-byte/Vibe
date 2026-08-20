5d00594623fc27049a123be768bb8d3cd21d0a08
umeshhnandavja-byte <umeshhnanda.vja@gmail.com>
2026-08-20 20:06:07 +0530

    Click Event

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,7 +1,12 @@
 function Button(){
+
+    let count = 0;
+
+    const handleClick = (event) => event.target.textContent = "Ouch!";
+
     return(
         <>
-           <button className="button">Click Me</button> 
+           <button className="button" onDoubleClick={(event) => handleClick(event)}>Click Me</button>
         </>
     );
 }
